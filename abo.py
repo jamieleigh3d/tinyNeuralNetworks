6bdf0ff912430356f9d60f476adab365d0795365
Jamie-Leigh <106563728+jamieleigh3d@users.noreply.github.com>
2023-08-17 14:25:01 -0700

    Load all objects

--- abo.py
+++ abo.py
@@ -6,18 +6,38 @@ domain_tag = 'domain_name'
 main_image_id_tag = 'main_image_id'
 item_id_tag = 'item_id'
 item_name_tag = 'item_name'
+item_keywords_tag = 'item_keywords'
 image_id_tag = 'image_id'
 height_tag = 'height'
 width_tag = 'width'
 path_tag = 'path'
 
-
+def is_keyword(sub_string, full_string):
+    return all(char in full_string for char in sub_string)
+    
 def filter(json_obj):
+    name = get_itemname_for_object(json_obj)
+    #if name is None:
+    #    return False
+    #print(name)
+    keywords = get_keywords_for_object(json_obj)
+    #if keywords is None:
+    #    return False
+    #print(keywords)
+    key = 'drink'
+    kw = 'drink'
+    dotcom = json_obj[domain_tag] == 'amazon.com'
     if domain_tag in json_obj:
-        return json_obj[domain_tag] == 'amazon.com'
+        return dotcom#(key in name or kw in keywords)
     return False
 
 
+def get_keywords_for_object(obj):
+    if item_keywords_tag in obj:
+        keywords = obj[item_keywords_tag]
+        return [kw['value'] for kw in keywords]
+    return None
+    
 def get_itemname_for_object(obj):
     if item_name_tag in obj:
         names = obj[item_name_tag]
@@ -37,16 +57,18 @@ def get_filepath_for_object(obj, image_list):
     
 
 def load_objects():
-
-    listings_filepath = "data/abo/listings/metadata/listings_0.json"
+    suffixes = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
     
     obj_data = []
 
-    with open(listings_filepath, 'r') as f:
-        for line in f:
-            json_obj = json.loads(line)
-            if filter(json_obj):
-                obj_data.append(json_obj)
+    for s in suffixes:
+        listings_filepath = f"data/abo/listings/metadata/listings_{s}.json"
+
+        with open(listings_filepath, 'r') as f:
+            for line in f:
+                json_obj = json.loads(line)
+                if filter(json_obj):
+                    obj_data.append(json_obj)
     return obj_data
     
 def load_images():
