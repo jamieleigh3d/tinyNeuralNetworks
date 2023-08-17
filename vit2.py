e9a5f15f76250ae2f2c0462b3a7e51508b6d3e05
Jamie-Leigh <106563728+jamieleigh3d@users.noreply.github.com>
2023-08-16 18:46:05 -0700

    Oops

--- vit2.py
+++ vit2.py
@@ -35,10 +35,11 @@ def main():
     model = MyViT((1, 28, 28), n_patches=7, n_blocks=2, hidden_d=8, n_heads=2, out_d=10).to(device)
     N_EPOCHS = 20
     LR = 0.005
-
-    # Training loop
+    
     optimizer = Adam(model.parameters(), lr=LR)
     criterion = CrossEntropyLoss()
+    
+    # Training loop
     for epoch in trange(N_EPOCHS, desc="Training"):
         train_loss = 0.0
         for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1} in training", leave=False):
@@ -52,7 +53,6 @@ def main():
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            break
 
         print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
 
