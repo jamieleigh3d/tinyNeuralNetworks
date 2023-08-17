8575bca870db3d5762fbb97c3d794e4013f8fb77
Jamie-Leigh <106563728+jamieleigh3d@users.noreply.github.com>
2023-08-17 00:36:19 -0700

    Refactor ImageLossFrame from abo-vit

---
+++ ImageFrame.py
@@ -0,0 +1,125 @@
+import wx
+
+from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+
+class ImageLossFrame(wx.Frame):
+    def __init__(self, parent, title):
+        super().__init__(parent, title=title, size=(2100, 1200))
+        
+        self.panel = wx.Panel(self)
+        
+        self.SetPosition((100, 10))  # Set X and Y coordinates
+                
+        self.figure1, self.plot1 = self.create_plot()
+        self.figure2, self.plot2 = self.create_plot()
+        self.figure3, self.plot3 = self.create_plot()
+        self.canvas1 = FigureCanvas(self.panel, -1, self.figure1)
+        self.canvas2 = FigureCanvas(self.panel, -1, self.figure2)
+        self.canvas3 = FigureCanvas(self.panel, -1, self.figure3)
+        
+        self.rows = 3
+        self.cols = 12
+        # Using a FlexGridSizer for the image grid
+        self.grid = wx.FlexGridSizer(self.rows, self.cols, 10, 10)
+        for i in range(self.cols):
+            self.grid.AddGrowableCol(i, 1)
+        for i in range(self.rows):
+            self.grid.AddGrowableRow(i, 1)
+        
+        # Create image placeholders
+        self.image_boxes = [wx.StaticBitmap(self.panel) for _ in range(self.rows*self.cols)]  
+        for box in self.image_boxes:
+            self.grid.Add(box, flag=wx.EXPAND)
+        
+        self.vbox = wx.BoxSizer(wx.VERTICAL)
+        self.vbox.Add(self.grid, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
+        
+        self.hbox = wx.BoxSizer(wx.HORIZONTAL)
+        self.hbox.Add(self.canvas1, 1, wx.EXPAND)
+        self.hbox.Add(self.canvas2, 1, wx.EXPAND)
+        self.hbox.Add(self.canvas3, 1, wx.EXPAND)
+        
+        self.vbox.Add(self.hbox)
+        
+        self.panel.SetSizer(self.vbox)
+        
+        self.sc = None
+        
+        self.done = False
+        self.thread = None
+        
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        
+    def OnClose(self, event):
+        # Do cleanup here, stop threads, release resources
+        self.Destroy()
+        wx.GetApp().ExitMainLoop()
+        self.done = True
+        if self.thread is not None:
+            self.thread.join()
+
+    def create_plot(self):
+        figure = Figure()
+        plot = figure.add_subplot(111)
+        return figure, plot
+    
+    def update_plot(self, total_losses, r_losses, kld_losses, d_losses):
+        self.plot1.clear()
+        self.plot1.plot(r_losses)
+        self.plot1.plot(total_losses)
+        
+        #self.plot1.set_yscale('log')
+        self.plot1.set_xlabel('Epoch')
+        self.plot1.set_ylabel('Loss')
+        self.plot1.set_title('Total/Recon Losses')
+        self.canvas1.draw()
+        
+        self.plot2.clear()
+        self.plot2.plot(kld_losses)
+        
+        self.plot2.set_xlabel('Epoch')
+        self.plot2.set_ylabel('Loss')
+        self.plot2.set_title('KLD Losses')
+        self.canvas2.draw()        
+        
+        self.plot3.clear()
+        self.plot3.plot(d_losses)
+        
+        self.plot3.set_xlabel('Epoch')
+        self.plot3.set_ylabel('Loss')
+        self.plot3.set_title('Perceptual Losses')
+        self.canvas3.draw()
+        
+
+    def PIL_to_wxBitmap(self, pil_image):
+        width, height = pil_image.size
+        buffer = pil_image.convert("RGB").tobytes()
+        wx_image = wx.Image(width, height, buffer)
+        bitmap = wx_image.ConvertToBitmap()  # This converts it to a wx.Bitmap
+        return bitmap
+
+    def show_images(self, idx_images, total_losses, r_losses, kld_losses, d_losses, latent_vectors=None):
+        
+        self.update_plot(total_losses, r_losses, kld_losses, d_losses)
+        
+        for (idx, img) in idx_images:
+            width, height = (128,128) #img.size
+            img = img.resize((width, height))
+            bitmap = self.PIL_to_wxBitmap(img)
+            if idx >= 0 and idx < len(self.image_boxes):
+                self.image_boxes[idx].SetBitmap(bitmap)
+        
+        if latent_vectors is not None:
+            tsne = TSNE(random_state=seed, n_components=2, perplexity=1, n_iter=300)
+            tsne_results = tsne.fit_transform(latent_vectors)
+        
+            if self.sc is not None:
+                self.sc.remove()
+            self.sc = plt.scatter(tsne_results[:, 0], tsne_results[:, 1])
+            plt.xlabel('t-SNE Component 1')
+            plt.ylabel('t-SNE Component 2')
+            plt.title('t-SNE visualization of latent space')
+            plt.draw()
+            plt.show()
--- abo-vit.py
+++ abo-vit.py
@@ -14,14 +14,14 @@ import torchvision.models as tvm
 import random
 from PIL import Image as PILImage
 from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
-from matplotlib.figure import Figure
+
 
 import nltk
 from nltk.tokenize import WhitespaceTokenizer
 from string import punctuation
 import signal
+
+from ImageFrame import ImageLossFrame
 
 import sys
 
@@ -106,85 +106,6 @@ def get_filepath_for_object(obj, image_list):
     return image_filepath
     
 
-class ImageLoaderFrame(wx.Frame):
-    def __init__(self, parent, title):
-        super().__init__(parent, title=title, size=(2100, 1200))
-        
-        self.panel = wx.Panel(self)
-        
-        self.SetPosition((100, 10))  # Set X and Y coordinates
-                
-        self.figure1, self.plot1 = self.create_plot()
-        self.figure2, self.plot2 = self.create_plot()
-        self.figure3, self.plot3 = self.create_plot()
-        self.canvas1 = FigureCanvas(self.panel, -1, self.figure1)
-        self.canvas2 = FigureCanvas(self.panel, -1, self.figure2)
-        self.canvas3 = FigureCanvas(self.panel, -1, self.figure3)
-        
-        self.rows = 3
-        self.cols = 12
-        # Using a FlexGridSizer for the image grid
-        self.grid = wx.FlexGridSizer(self.rows, self.cols, 10, 10)
-        for i in range(self.cols):
-            self.grid.AddGrowableCol(i, 1)
-        for i in range(self.rows):
-            self.grid.AddGrowableRow(i, 1)
-        
-        # Create image placeholders
-        self.image_boxes = [wx.StaticBitmap(self.panel) for _ in range(self.rows*self.cols)]  
-        for box in self.image_boxes:
-            self.grid.Add(box, flag=wx.EXPAND)
-        
-        self.vbox = wx.BoxSizer(wx.VERTICAL)
-        self.vbox.Add(self.grid, proportion=1, flag=wx.ALL | wx.EXPAND, border=10)
-        
-        self.hbox = wx.BoxSizer(wx.HORIZONTAL)
-        self.hbox.Add(self.canvas1, 1, wx.EXPAND)
-        self.hbox.Add(self.canvas2, 1, wx.EXPAND)
-        self.hbox.Add(self.canvas3, 1, wx.EXPAND)
-        
-        self.vbox.Add(self.hbox)
-        
-        self.panel.SetSizer(self.vbox)
-
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
-        
-    def create_plot(self):
-        figure = Figure()
-        plot = figure.add_subplot(111)
-        return figure, plot
-    
-    def update_plot(self, total_losses, r_losses, kld_losses, d_losses):
-        self.plot1.clear()
-        self.plot1.plot(r_losses)
-        self.plot1.plot(total_losses)
-        
-        #self.plot1.set_yscale('log')
-        self.plot1.set_xlabel('Epoch')
-        self.plot1.set_ylabel('Loss')
-        self.plot1.set_title('Total/Recon Losses')
-        self.canvas1.draw()
-        
-        self.plot2.clear()
-        self.plot2.plot(kld_losses)
-        
-        self.plot2.set_xlabel('Epoch')
-        self.plot2.set_ylabel('Loss')
-        self.plot2.set_title('KLD Losses')
-        self.canvas2.draw()        
-        
-        self.plot3.clear()
-        self.plot3.plot(d_losses)
-        
-        self.plot3.set_xlabel('Epoch')
-        self.plot3.set_ylabel('Loss')
-        self.plot3.set_title('Perceptual Losses')
-        self.canvas3.draw()
-        
-    def OnClose(self, event):
-        # Do cleanup here, stop threads, release resources
-        self.Destroy()
-        wx.GetApp().ExitMainLoop()
 
 def image_to_tensor(img):
     """Convert a PIL image to a PyTorch tensor."""
@@ -199,38 +120,6 @@ def tensor_to_image(tensor_img):
     np_img = np.clip((tensor_img.numpy() * 255), 0, 255).astype(np.uint8)  # Convert tensor to [0, 255] range
     return PILImage.fromarray(np_img)
 
-def PIL_to_wxBitmap(pil_image):
-    width, height = pil_image.size
-    buffer = pil_image.convert("RGB").tobytes()
-    wx_image = wx.Image(width, height, buffer)
-    bitmap = wx_image.ConvertToBitmap()  # This converts it to a wx.Bitmap
-    return bitmap
-
-sc = None
-
-def show_images(frame, idx_images, total_losses, r_losses, kld_losses, d_losses, latent_vectors=None):
-    global sc
-    frame.update_plot(total_losses, r_losses, kld_losses, d_losses)
-    
-    for (idx, img) in idx_images:
-        width, height = (128,128) #img.size
-        img = img.resize((width, height))
-        bitmap = PIL_to_wxBitmap(img)
-        if idx >= 0 and idx < len(frame.image_boxes):
-            frame.image_boxes[idx].SetBitmap(bitmap)
-    
-    if latent_vectors is not None:
-        tsne = TSNE(random_state=seed, n_components=2, perplexity=1, n_iter=300)
-        tsne_results = tsne.fit_transform(latent_vectors)
-    
-        if sc is not None:
-            sc.remove()
-        sc = plt.scatter(tsne_results[:, 0], tsne_results[:, 1])
-        plt.xlabel('t-SNE Component 1')
-        plt.ylabel('t-SNE Component 2')
-        plt.title('t-SNE visualization of latent space')
-        plt.draw()
-        plt.show()
 
 def batches(array, batch_size):
     for i in range(0, len(array), batch_size):
@@ -515,6 +404,9 @@ def train(frame):
         kld_losses.append(kld_term)
         d_losses.append(p_term)
         
+        if frame is not None and frame.done:
+            return
+        
         if (epoch+1) % logging_interval == 0:
             print(f"Epoch {epoch+1}/{num_epochs}, Total Loss: {total_loss:.8f}, R_Loss: {r_term:.8f}, KLD Loss: {kld_term:.8f}, Perceptual Loss: {p_term:.8f}")
             
@@ -551,7 +443,7 @@ def train(frame):
                     show_image_list.append((i+frame.cols*2,pil_image))
                 
             
-            wx.CallAfter(show_images, frame, show_image_list, total_losses, r_losses, kld_losses, d_losses)#, latent_vectors)
+            wx.CallAfter(frame.show_images, show_image_list, total_losses, r_losses, kld_losses, d_losses)#, latent_vectors)
             if save_enabled:
                 if lowest_loss is None:
                     lowest_loss = total_loss+1
@@ -570,7 +462,7 @@ def start_training_thread(frame):
 
 if __name__ == "__main__":
     app = wx.App(False)
-    frame = ImageLoaderFrame(None, 'Image Processing GUI')
+    frame = ImageLossFrame(None, 'Image Processing GUI')
     frame.Show()
     frame.thread = start_training_thread(frame)
     
